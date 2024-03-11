1d76106165bed767efb37ad408dddf2c5565d641
Maxim <334890@gmail.com>
2024-03-11 11:07:52 +0600

    add alternative dnd mode

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,11 @@
 import type { Identifier } from "dnd-core";
 import type { FC } from "react";
 import { useRef } from "react";
-import { useDrag, useDrop } from "react-dnd";
+import { XYCoord, useDrag, useDrop } from "react-dnd";
 import { ItemTypes } from "../itemTypes";
 import { cn } from "../utils";
 import Delete from "./svg/Delete";
+import { useMaxWidth } from "../hooks/useMaxWidth";
 
 export interface CardProps {
   id: any;
@@ -27,6 +28,7 @@ export const Card: FC<CardProps> = ({
   moveCard,
   handleDeleteFile,
 }) => {
+  const isInline = useMaxWidth(1020);
   const ref = useRef<HTMLDivElement>(null);
   const [{ handlerId }, drop] = useDrop<
     DragItem,
@@ -39,6 +41,54 @@ export const Card: FC<CardProps> = ({
         handlerId: monitor.getHandlerId(),
       };
     },
+
+    hover(item: DragItem, monitor) {
+      if (!isInline) return;
+      if (!ref.current) return;
+
+      const dragIndex = item.index;
+      const hoverIndex = index;
+
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+
+      // Get vertical middle
+      const hoverMiddleX =
+        (hoverBoundingRect.right - hoverBoundingRect.left) / 2;
+
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset();
+
+      // Get pixels to the top
+      const hoverClientX = (clientOffset as XYCoord).x - hoverBoundingRect.left;
+
+      // Only perform the move when the mouse has crossed half of the items width
+
+      // Dragging right
+      if (dragIndex < hoverIndex && hoverClientX < hoverMiddleX) {
+        return;
+      }
+
+      // Dragging left
+      if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
+        return;
+      }
+
+      // Time to actually perform the action
+      moveCard(dragIndex, hoverIndex);
+
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex;
+    },
+
     drop(item: DragItem) {
       if (!ref.current) {
         return;
